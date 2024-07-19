c3dacbde7c99ea7472b5fb308c65030555e9b4bd
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-07-20 02:32:20 +0300

    2. Video - Variables

---
+++ 2-) Variables/app.py
@@ -0,0 +1,12 @@
+
+price = 10 #integer
+rating = 4.9 #float
+name = 'Mosh' #String
+is_published = True #boolean, True or False special words
+#Python is Case sensetive language
+price = 20
+print(price)
+
+patientName = 'Jhon Smith'
+patientAge = 20
+is_new = True
