ba20ae5518f5bb7dafbe8f40380b8c0cdc5a5dcb
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-07-23 01:23:43 +0300

    5. Video - Strings

---
+++ 5-)Strings/app.py
@@ -0,0 +1,26 @@
+course = "Python's Course for Beginners"
+#         01234                       -1
+
+course2 = 'Pythons Course for "Beginners"'
+
+course3 = ''' 
+ Hi John,
+ 
+ Here is our first email to you.
+ 
+ Thank you,
+ The support team
+ 
+ '''
+name = 'Jennifer'
+
+print(name[1:-1])
+
+#course - print whole string
+#course[0] - print first char
+#course[-1] - print last char
+#course[0:3] - print first 3 char
+#course[0:] - print whole string
+#course[1:] -start 2nd char and print whole string
+#course[:5] - python select start index auto 0 andt print first 5 char
+#course[:] - course[:] and course is the same thing. print whole string
