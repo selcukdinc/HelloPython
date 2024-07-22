39ee52b41a35be32f216b602c25d75fd71099604
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-07-22 10:30:15 +0300

    4. Vide0 - Type Conversion

---
+++ 4-)TypeConversions/app.py
@@ -0,0 +1,16 @@
+'''
+birth_year = input('Birth year: ')
+print(type(birth_year))
+age = 2024 - int(birth_year)
+print(type(age))
+#int()
+#float()
+#bool()
+print(age)
+'''
+
+# Ask a usr their weight (in pounds), convert it to kilograms and print on the terminal
+
+weight_lbs = input('enter your weight (lbs) : ')
+weight_kg = 0.4535 * float(weight_lbs)
+print(weight_kg)
