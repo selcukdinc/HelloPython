10830218ebb3d8c74a076b7f5a869a5da306576e
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-07-25 00:45:25 +0300

    7. Video - String Methods

---
+++ 7-)StringMethods/app.py
@@ -0,0 +1,11 @@
+course = 'Python for Beginners'
+#print(len(course))
+'''
+print(course.upper())
+print(course.lower())
+print(course)
+'''
+#print(course.find('Beginners'))
+#print(course.replace('Beginners', 'Absolute Beginners'))
+#print(course.replace('P', 'J'))
+print('python' in course)
