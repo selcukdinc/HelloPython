36231d50c97a7953a90cd80447197f3c95fa9200
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-07-26 01:29:02 +0300

    8. Video - Arithmetic Operators and 9. Video Operator Precedence

---
+++ 8-)ArithmeticOperators/app.py
@@ -0,0 +1,13 @@
+# 8. Video Arithmetic Operators and 9. Video Operator Precedence merged
+x = 10
+x += 3
+print(x)
+
+# 9. Video Operator Precedence
+
+x = (10 + 3) * 2 ** 2
+print(x)
+# parenthesis
+# exponetiation 2**2
+# multiplacition or division
+# addition or subtraction
