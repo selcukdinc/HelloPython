dab460c4080e77f0a27141f5a37f124bc3c6197e
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-07-28 01:45:58 +0300

    11. Video - If Statements

---
+++ 11-)IfStatements/app.py
@@ -0,0 +1,23 @@
+'''
+is_hot = False
+is_cold = False
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's a lovely day")
+print("Enjoy your day")
+'''
+
+print("House price is 1M$")
+housePrice = 1000000
+is_good_credit = False
+if is_good_credit:
+    print("Oh i see, your a good credit user")
+    print(f"your paynemt is ${housePrice * 0.1}")
+else:
+    print("Oh! i prepared your documents")
+    print(f"your paynemt is ${housePrice * 0.2}")
