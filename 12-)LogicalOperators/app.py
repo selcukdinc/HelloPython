a729ddd29db6d5d0bc88477d96eb3f87093e3ba8
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-07-29 20:14:44 +0300

    12. Video Logical Operators

---
+++ 12-)LogicalOperators/app.py
@@ -0,0 +1,13 @@
+'''
+has_high_income = False
+has_good_credit = True
+
+if has_high_income or has_good_credit:
+    print("Eligible for loan")
+'''
+
+has_good_credit = True
+has_criminal_record = True
+
+if has_good_credit and not has_criminal_record:
+    print("Eligible for loan")
