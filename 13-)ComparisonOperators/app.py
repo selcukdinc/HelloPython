535805f0fef2eb0cd9cea51dc5bb64ef697b6d71
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-08-05 03:27:47 +0300

    13. Video Comparison Operators

---
+++ 13-)ComparisonOperators/app.py
@@ -0,0 +1,18 @@
+'''
+temperature = 3
+
+if temperature > 30:
+    print("It's a hot day")
+elif temperature < 10:
+    print("It's a cold day")
+else:
+    print("It's neither hot nor cold")
+'''
+
+username = "asdawdsadaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+if len(username) < 3:
+    print("name must be at least 3 characters")
+elif len(username) > 50:
+    print("name can be a maximum of 50 characters")
+else:
+    print("name looks good!")
