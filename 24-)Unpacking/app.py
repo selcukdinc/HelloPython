ebadc63a335d3f116f90bec75f690f5f19b2bba3
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-08-30 23:21:42 +0300

    24. Video - Unpacking

---
+++ 24-)Unpacking/app.py
@@ -0,0 +1,13 @@
+coordinates = (1, 2, 3)
+# coordinates[0] * coordinates[1] * coordinates[2]
+'''
+x = coordinates[0]
+y = coordinates[1]
+z = coordinates[2]
+# x * y * z
+'''
+# this is a unpacking and not special to tuples, also uses lists
+x, y, z = coordinates
+
+coordinates_lists = [1, 2, 3]
+x2, y2, z2 = coordinates_lists
