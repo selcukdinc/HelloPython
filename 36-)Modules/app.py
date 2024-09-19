36e557756e2ae71f057fec1d2a445c5e619df7cb
Selçuk DİNÇ <09.mesgul_dosya@icloud.com>
2024-09-20 00:50:34 +0300

    36. Video - Modules

---
+++ 36-)Modules/app.py
@@ -0,0 +1,10 @@
+import converters
+from converters import kg_to_lbs
+import utils
+
+print(kg_to_lbs(100))
+
+print(converters.kg_to_lbs(70))
+numbers = [2, 3, 1, 4, 5, 6, 0]
+
+print(utils.find_max(numbers))
